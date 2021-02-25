94112862ab5bcdd633a998e84b3935b822ae5254
hibiki1228 <devkimadore1228@gmail.com>
2021-02-25 18:24:37 +0900

    test

--- music/beep.py
+++ music/beep.py
@@ -1,6 +1,7 @@
 import pyaudio
 import numpy as np
 
+
 # 音声を出力するためのストリームを開く --- (*1)
 p =  pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100,frames_per_buffer=1024, output=True)
