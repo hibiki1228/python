66385b32d9b09054f99b8ef5788393347c05724e
hibiki1228 <devkimadore1228@gmail.com>
2021-04-11 14:01:00 +0900

    add flask file

--- flask/maltiplication.py
+++ flask/maltiplication.py
@@ -8,8 +8,26 @@ def root():
     #HTMLでWebフォームを記述 --- (*2)
     return """
     <html><body>
-    <form action="/calc" method="post">
+    <form action="/add" method="post">
+        <input type="text" name="a"> +
+        <input type="text" name="b">
+        <input type="submit" value="計算">
+    </form>
+
+    <form action="/mainus" method="post">
+        <input type="text" name="a"> -
+        <input type="text" name="b">
+        <input type="submit" value="計算">
+    </form>
+    
+    <form action="/addadd" method="post">
         <input type="text" name="a"> ×
+        <input type="text" name="b">
+        <input type="submit" value="計算">
+    </form>
+    
+    <form action="/decre" method="post">
+        <input type="text" name="a"> ÷
         <input type="text" name="b">
         <input type="submit" value="計算">
     </form>
@@ -17,11 +35,30 @@ def root():
 
 
 #フォームの値を受け取って結果を表示 --- (*3)
-@app.route("/calc", methods=["post"])
-def calc():
+@app.route("/add", methods=["post"])
+def add():
+    a = int(request.form.get("a"))
+    b = int(request.form.get("b"))
+    r = a + b
+    return "<h1>答えは…" + str(r) + "</h1>"
+
+@app.route("/mainus", methods=["post"])
+def mainus():
+    a = int(request.form.get("a"))
+    b = int(request.form.get("b"))
+    r = a - b
+    return "<h1>答えは…" + str(r) + "</h1>"
+@app.route("/addadd", methods=["post"])
+def addadd():
     a = int(request.form.get("a"))
     b = int(request.form.get("b"))
     r = a * b
+    return "<h1>答えは…" + str(r) + "</h1>"
+@app.route("/decre", methods=["post"])
+def decre():
+    a = int(request.form.get("a"))
+    b = int(request.form.get("b"))
+    r = a / b
     return "<h1>答えは…" + str(r) + "</h1>"
 
 
